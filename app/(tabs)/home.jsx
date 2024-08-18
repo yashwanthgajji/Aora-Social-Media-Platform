8d52e932fba9be850556d214802845c0ebf70ff9
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-08-18 01:01:44 -0400

    trending card with animations

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -6,12 +6,13 @@ import {images} from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
-import { getAllPosts } from '../../lib/appwrite'
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppWrite from '../../lib/useAppWrite'
 import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
   const {data: posts, refetch} = useAppWrite(getAllPosts)
+  const {data: latestPosts} = useAppWrite(getLatestPosts)
 
   const [refreshing, setRefreshing] = useState(false)
   const onRefresh = async () => {
@@ -49,7 +50,7 @@ const Home = () => {
             <View className="w-full flex-1 pt-5 pb-8">
               <Text className="text-gray-100 text-lg font-pregular mb-3">Latest Videos</Text>
               <Trending 
-                posts={[{id: 1}, {id: 2}, {id:3}] ?? []}
+                posts={latestPosts ?? []}
               />
             </View>
           </View>
